6e0658da9c0a3f4c5dcc5e98974f2e159c997256
mini0n <mini0n.000@gmail.com>
2019-06-07 01:58:29 -0500

    rubocop fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
+# Application Controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "ola k ase?"
+    render html: 'ola k ase?'
   end
-
 end
