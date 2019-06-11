26113194a311e641461a4b97fffccf275bff3997
mini0n <mini0n.000@gmail.com>
2019-06-11 06:20:51 -0500

    StaticPages controllers routes test

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe 'controller routes' do
+    context 'GET home' do
+      it 'returns 200' do
+        get :home
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'GET help' do
+      it 'returns 200' do
+        get :help
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'GET about' do
+      it 'returns 200' do
+        get :about
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
